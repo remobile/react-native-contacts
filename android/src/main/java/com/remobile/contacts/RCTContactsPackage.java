180ea9988c535bc6e9e1050b523dc2c780d7c1de
fov42550564 <42550564@qq.com>
2017-10-16 11:30:50 +0800

    update to 1.0.4

--- android/src/main/java/com/remobile/contacts/RCTContactsPackage.java
+++ android/src/main/java/com/remobile/contacts/RCTContactsPackage.java
@@ -18,7 +18,6 @@ public class RCTContactsPackage implements ReactPackage {
         );
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
